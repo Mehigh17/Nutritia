2b17b1651a33763ef089d43ba04955aeb9f16bbd
Mihai <kazepworld@gmail.com>
2019-04-05 23:55:14 +0200

    Add the request executer interface

---
+++ Nutritia/Nutritia/Services/IRequestExecuter.cs
@@ -0,0 +1,17 @@
+﻿using Nutritia.Models;
+using System.Threading.Tasks;
+
+namespace Nutritia.Services
+{
+    public interface IRequestExecuter
+    {
+
+        /// <summary>
+        /// Get a product information by passing the barcode.
+        /// </summary>
+        /// <param name="barCode">The barcode of the product</param>
+        /// <returns>A product</returns>
+        Task<Product> GetProduct(string barCode);
+
+    }
+}
