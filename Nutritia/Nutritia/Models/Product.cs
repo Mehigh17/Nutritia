bd15997ec7991eec3ae7eb6a97e7f69f06c45d6c
Mihai <kazepworld@gmail.com>
2019-04-06 23:26:29 +0200

    WIP: Create the API models

---
+++ Nutritia/Nutritia/Models/Product.cs
@@ -0,0 +1,22 @@
+﻿using Newtonsoft.Json;
+using System.Collections.Generic;
+
+namespace Nutritia.Models
+{
+    public class Product
+    {
+
+        [JsonProperty("brands_tags")]
+        public List<string> BrandTags { get; set; }
+
+        [JsonProperty("serving_size")]
+        public string ServingSize { get; set; }
+
+        [JsonProperty("image_url")]
+        public string ImageUrl { get; set; }
+
+        [JsonProperty("generic_name")]
+        public string GenericName { get; set; }
+
+    }
+}
