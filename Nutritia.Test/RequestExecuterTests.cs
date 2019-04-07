3627259bfb2e9d9ce0675a5eaa08d687834cb715
Mihai <kazepworld@gmail.com>
2019-04-07 22:37:32 +0200

    Add the tests project

---
+++ Nutritia.Test/RequestExecuterTests.cs
@@ -0,0 +1,25 @@
+﻿using Nutritia.Services;
+using System.Threading.Tasks;
+using Xunit;
+
+namespace Nutritia.Test
+{
+    public class RequestExecuterTests : IClassFixture<RequestExecuter>
+    {
+
+        private readonly RequestExecuter _requestExecuter;
+
+        public RequestExecuterTests(RequestExecuter requestExecuter)
+        {
+            _requestExecuter = requestExecuter;
+        }
+
+        [Fact]
+        public async Task GetProduct_ValidBarcodeGiven_ShouldPass()
+        {
+            var product = await _requestExecuter.GetProduct("3274080005003");
+            Assert.NotNull(product);
+        }
+
+    }
+}
