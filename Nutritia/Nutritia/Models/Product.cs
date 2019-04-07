bc64dd7d0c309c99823ef1e724b97952b96b6449
Mihai <kazepworld@gmail.com>
2019-04-08 01:23:15 +0200

    Add more properties about the product into the model

--- Nutritia/Nutritia/Models/Product.cs
+++ Nutritia/Nutritia/Models/Product.cs
@@ -1,4 +1,6 @@
 ﻿using Newtonsoft.Json;
+using Nutritia.Converters;
+using System;
 using System.Collections.Generic;
 
 namespace Nutritia.Models
@@ -17,6 +19,49 @@ namespace Nutritia.Models
 
         [JsonProperty("generic_name")]
         public string GenericName { get; set; }
+
+        [JsonProperty("product_name")]
+        public string ProductName { get; set; }
+
+        [JsonProperty("quantity")]
+        public string Quantity { get; set; }
+
+        [JsonProperty("image_nutrition_url")]
+        public string NutritionImageUrl { get; set; }
+
+        [JsonProperty("image_ingredients_url")]
+        public string IngredientsImageUrl { get; set; }
+
+        [JsonProperty("allergens_hierarchy")]
+        public List<string> AllergensHierarchy { get; set; }
+
+        /// <summary>
+        /// This is a string with categories separated by commas
+        /// </summary>
+        [JsonProperty("categories")]
+        public string Categories { get; set; }
+
+        /// <summary>
+        /// This is a string with the allergens spearated by commas
+        /// </summary>
+        [JsonProperty("allergens")]
+        public string Allergens { get; set; }
+
+        [JsonProperty("created_t")]
+        [JsonConverter(typeof(UnixEpochDateTimeConverter))]
+        public DateTime CreatedDateTime { get; set; }
+
+        [JsonProperty("nutrient_levels")]
+        public Dictionary<string, object> NutrientLevels { get; set; }
+
+        [JsonProperty("ingredients_text")]
+        public string IngredientsTranscript { get; set; }
+
+        [JsonProperty("_keywords")]
+        public List<string> Keywords { get; set; }
+
+        [JsonProperty("nutriments")]
+        public Dictionary<string, object> Nutriments { get; set; }
 
     }
 }
