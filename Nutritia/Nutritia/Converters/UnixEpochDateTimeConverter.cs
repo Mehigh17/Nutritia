5dbe5af4ba1b166a71afabb0f088557207ba797a
Mihai <kazepworld@gmail.com>
2019-04-08 01:22:51 +0200

    Add unix epoch to date time converter

---
+++ Nutritia/Nutritia/Converters/UnixEpochDateTimeConverter.cs
@@ -0,0 +1,31 @@
+﻿using Newtonsoft.Json;
+using System;
+
+namespace Nutritia.Converters
+{
+    public class UnixEpochDateTimeConverter : JsonConverter
+    {
+
+        public override bool CanConvert(Type objectType)
+        {
+            return objectType == typeof(DateTime);
+        }
+
+        public override object ReadJson(JsonReader reader, Type objectType, object existingValue, JsonSerializer serializer)
+        {
+            var dateTime = new DateTime(1970, 1, 1, 0, 0, 0, DateTimeKind.Utc);
+            if (long.TryParse(reader.Value.ToString(), out var epochTime))
+            {
+                dateTime = dateTime.AddSeconds(epochTime);
+            }
+
+            return dateTime;
+        }
+
+        public override void WriteJson(JsonWriter writer, object value, JsonSerializer serializer)
+        {
+            throw new NotImplementedException();
+        }
+
+    }
+}
