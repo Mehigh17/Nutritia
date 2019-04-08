6b91861f2a8c5deb218c4c49f857445a92258e45
Mihai <kazepworld@gmail.com>
2019-04-08 23:30:07 +0200

    Fetch the product and populate the product vmwhen barcode is scanned

--- Nutritia/Nutritia/ViewModels/HomeViewModel.cs
+++ Nutritia/Nutritia/ViewModels/HomeViewModel.cs
@@ -1,13 +1,56 @@
-﻿namespace Nutritia.ViewModels
+﻿using Nutritia.Exceptions;
+using Nutritia.Services;
+using Nutritia.Views;
+using System;
+using Xamarin.Forms;
+
+namespace Nutritia.ViewModels
 {
     public class HomeViewModel : BaseViewModel
     {
 
+        private ProductDetailViewModel _productVm;
+        public ProductDetailViewModel ProductVm
+        {
+            get => _productVm;
+            set => SetProperty(ref _productVm, value);
+        }
+
         public HomeViewModel()
         {
             Title = "Home";
+            ProductVm = new ProductDetailViewModel();
 
+            MessagingCenter.Subscribe<HomePage, string>(this, "BarcodeScanned", OnBarcodeReceived);
+        }
 
+        private async void OnBarcodeReceived(object sender, string barcode)
+        {
+            if(!string.IsNullOrEmpty(barcode))
+            {
+                ProductVm.IsBusy = true;
+                try
+                {
+                    var reqExec = new RequestExecuter();
+                    var product = await reqExec.GetProduct(barcode);
+
+                    ProductVm.ImgUrl = product.ImageUrl;
+                    ProductVm.ProductName = product.ProductName;
+                }
+                catch(ProductNotFoundException)
+                {
+                    ProductVm.ImgUrl = string.Empty;
+                    ProductVm.ProductName = "Product not found.";
+                }
+                catch(Exception e)
+                {
+                    throw e;
+                }
+                finally
+                {
+                    ProductVm.IsBusy = false;
+                }
+            }
         }
 
     }
