4f82c39b9135288be869593f5f496bc224997ddd
Mihai <kazepworld@gmail.com>
2019-04-08 23:29:24 +0200

    Add the product detail view and viewmodel

---
+++ Nutritia/Nutritia/ViewModels/ProductDetailViewModel.cs
@@ -0,0 +1,26 @@
+﻿namespace Nutritia.ViewModels
+{
+    public class ProductDetailViewModel : BaseViewModel
+    {
+
+        private string _imgUrl;
+        public string ImgUrl
+        {
+            get => _imgUrl;
+            set => SetProperty(ref _imgUrl, value);
+        }
+
+        private string _productName;
+        public string ProductName
+        {
+            get => _productName;
+            set => SetProperty(ref _productName, value);
+        }
+
+        public ProductDetailViewModel()
+        {
+
+        }
+
+    }
+}
---
+++ Nutritia/Nutritia/Views/ProductDetailView.xaml.cs
@@ -0,0 +1,15 @@
+﻿
+using Xamarin.Forms;
+using Xamarin.Forms.Xaml;
+
+namespace Nutritia.Views
+{
+    [XamlCompilation(XamlCompilationOptions.Compile)]
+    public partial class ProductDetailView : ContentView
+    {
+        public ProductDetailView()
+        {
+            InitializeComponent();
+        }
+    }
+}
