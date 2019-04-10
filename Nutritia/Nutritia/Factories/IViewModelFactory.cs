6b7919ba72c87b908dacb14bdd681be7ce359c34
Mihai <kazepworld@gmail.com>
2019-04-11 01:18:43 +0200

    WIP: Add view models factory

---
+++ Nutritia/Nutritia/Factories/IViewModelFactory.cs
@@ -0,0 +1,12 @@
+﻿using Nutritia.Models;
+using Nutritia.ViewModels;
+
+namespace Nutritia.Factories
+{
+    public interface IViewModelFactory
+    {
+
+        ProductDetailViewModel CreateProductDetailViewModel(Product product);
+
+    }
+}
---
+++ Nutritia/Nutritia/Factories/ViewModelFactory.cs
@@ -0,0 +1,29 @@
+﻿using Nutritia.Models;
+using Nutritia.ViewModels;
+using System;
+using System.Collections.ObjectModel;
+using System.Linq;
+
+namespace Nutritia.Factories
+{
+    public class ViewModelFactory : IViewModelFactory
+    {
+        public ProductDetailViewModel CreateProductDetailViewModel(Product product)
+        {
+            var vm = new ProductDetailViewModel
+            {
+                ImgUrl = product.ImageUrl,
+                ProductName = product.ProductName,
+                NutritionGradeTag = product.NutritionGradeTags.FirstOrDefault(),
+            };
+
+            var nutriments = new ObservableCollection<Tuple<string, string>>(product.Nutriments.Where(n => !string.IsNullOrEmpty(n.Value.ToString()))
+                                                                                               .Select(n => new Tuple<string, string>(n.Key.ToString(), n.Value.ToString())));
+
+            
+            vm.Nutriments = nutriments;
+
+            return vm;
+        }
+    }
+}
