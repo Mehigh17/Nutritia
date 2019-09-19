65f1f9e4eea8243672395adfbd121ec6205110e2
Mihai Stan <stanmihai17cs@gmail.com>
2019-09-19 18:42:34 +0200

    Remove unused nutrition tag

--- Nutritia/Nutritia/Factories/ViewModelFactory.cs
+++ Nutritia/Nutritia/Factories/ViewModelFactory.cs
@@ -14,15 +14,10 @@ namespace Nutritia.Factories
             {
                 ImgUrl = product.ImageUrl,
                 ProductName = product.ProductName,
-                NutritionGradeTag = product.NutritionGradeTags.FirstOrDefault(),
             };
 
             var nutriments = new ObservableCollection<Tuple<string, string>>(product.Nutriments.Where(n => !string.IsNullOrEmpty(n.Value.ToString()))
                                                                                                .Select(n => new Tuple<string, string>(n.Key.ToString(), n.Value.ToString())));
-
-            
-            vm.Nutriments = nutriments;
-
             return vm;
         }
     }
