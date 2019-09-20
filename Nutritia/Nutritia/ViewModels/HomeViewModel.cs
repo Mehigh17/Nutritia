b99a258ad2e904b5b9cc96ed85cb4177aa4935bf
Mihai Stan <stanmihai17cs@gmail.com>
2019-09-20 13:25:58 +0200

    Add search records on the home page

--- Nutritia/Nutritia/ViewModels/HomeViewModel.cs
+++ Nutritia/Nutritia/ViewModels/HomeViewModel.cs
@@ -1,9 +1,11 @@
-using Nutritia.Exceptions;
-using Nutritia.Factories;
+﻿using Nutritia.Factories;
+using Nutritia.Models;
+using Nutritia.Repositories;
 using Nutritia.Services;
 using Nutritia.Views;
 using System;
 using System.Collections.ObjectModel;
+using System.Threading.Tasks;
 using Xamarin.Forms;
 
 namespace Nutritia.ViewModels
@@ -18,20 +20,30 @@ namespace Nutritia.ViewModels
             set => SetProperty(ref _productRecords, value);
         }
 
+        private readonly IRecordRepository _recordRepo;
         private readonly IRequestExecuter _requestExecuter;
         private readonly IViewModelFactory _viewModelFactory;
 
         public HomeViewModel()
         {
+            _recordRepo = DependencyService.Resolve<IRecordRepository>() ?? throw new ArgumentNullException(nameof(_recordRepo));
             _requestExecuter = DependencyService.Resolve<RequestExecuter>() ?? throw new ArgumentNullException(nameof(_requestExecuter));
             _viewModelFactory = DependencyService.Resolve<IViewModelFactory>() ?? throw new ArgumentNullException(nameof(_requestExecuter));
 
             Title = "Home";
 
-            MessagingCenter.Subscribe<HomePage, string>(this, "BarcodeScanned", OnBarcodeReceived);
+            MessagingCenter.Subscribe<HomePage, string>(this, "BarcodeScanned", async (s, c) => await OnBarcodeReceived(c));
+
+            var records = _recordRepo.GetRecords();
+            records.ForEach(async (r) =>
+            {
+                var product = await _requestExecuter.GetProduct(r.Bardcode);
+                var vm = _viewModelFactory.CreateProductRecordViewModel(r, product);
+                ProductRecords.Add(vm);
+            });
         }
 
-        private async void OnBarcodeReceived(object sender, string barcode)
+        private async Task OnBarcodeReceived(string barcode)
         {
             if(!string.IsNullOrEmpty(barcode))
             {
@@ -39,11 +51,14 @@ namespace Nutritia.ViewModels
                 try
                 {
                     var product = await _requestExecuter.GetProduct(barcode);
-                    //ProductVm = _viewModelFactory.CreateProductDetailViewModel(product);
-                }
-                catch(ProductNotFoundException)
-                {
-                    //ProductVm = null;
+                    var record = new ProductRecord
+                    {
+                        Bardcode = barcode,
+                        SearchedOn = DateTime.Now
+                    };
+                    _recordRepo.AddRecord(record);
+                    var vm = _viewModelFactory.CreateProductRecordViewModel(record, product);
+                    ProductRecords.Add(vm);
                 }
                 catch(Exception e)
                 {
