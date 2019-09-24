445e83f239f32d7b3d56a2545118f4a3f161e95e
Mihai Stan <stanmihai17cs@gmail.com>
2019-09-24 12:31:54 +0200

    Add source code url on about page

--- Nutritia/Nutritia/ViewModels/AboutViewModel.cs
+++ Nutritia/Nutritia/ViewModels/AboutViewModel.cs
@@ -7,13 +7,19 @@ namespace Nutritia.ViewModels
 {
     public class AboutViewModel : PageViewModel
     {
+
+        private ICommand _showSourceCommand;
+        public ICommand ShowSourceCommand
+        {
+            get => _showSourceCommand;
+            set => SetProperty(ref _showSourceCommand, value);
+        }
+
         public AboutViewModel()
         {
             Title = "About";
 
-            OpenWebCommand = new Command(() => Device.OpenUri(new Uri("https://xamarin.com/platform")));
+            ShowSourceCommand = new Command(() => Device.OpenUri(new Uri("https://github.com/Mehigh17/Nutritia")));
         }
-
-        public ICommand OpenWebCommand { get; }
     }
 }
